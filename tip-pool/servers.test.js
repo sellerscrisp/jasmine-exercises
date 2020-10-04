abe6b7c18f760e1f1310cd39818ed03e9cf555e5
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-04 16:06:36 -0500

    tested all current servers.js functions

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,4 +1,4 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe("Servers test (with setup and tear-down)", function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -11,7 +11,26 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
 
-  afterEach(function() {
-    // teardown logic
+  it('should not add a new server to allServers on submitServerInfo()', function () {
+    serverNameInput.value = '';
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+
+  it('should update server table when updateServerTable() is called', function () {
+    submitServerInfo();
+    updateServerTable();
+
+    let serverTable = document.querySelectorAll('#serverTable tbody tr td');
+    expect(serverTable.length).toEqual(2);
+    expect(serverTable[0].innerText).toEqual('Alice');
+    expect(serverTable[1].innerText).toEqual('$0.00');
+  });
+
+  afterEach(function () {
+    serverId = 0;
+    allServers = {};
+    serverTbody.innerHTML = '';
   });
 });
