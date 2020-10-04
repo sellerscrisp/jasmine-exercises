2a1275d93fe86a3dd0445fba66e07af57d5b3bb1
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-04 18:06:05 -0500

    added more tests

--- tip-pool/helpers.test.js
+++ tip-pool/helpers.test.js
@@ -5,7 +5,7 @@ describe('Helpers test (with setup and tear-down)', function () {
         submitPaymentInfo();
     });
 
-    it('should accept the tip and total previous tip payments', function () {
+    it('should accept the tip and sum previous tip payments', function () {
         expect(sumPaymentTotal('tipAmt')).toEqual(40);
 
         billAmtInput.value = 100;
@@ -14,13 +14,42 @@ describe('Helpers test (with setup and tear-down)', function () {
         expect(sumPaymentTotal('tipAmt')).toEqual(60);
     });
 
-    it('should accept the bill and total previous bill payments', function () {
+    it('should accept the bill and sum previous bill payments', function () {
         expect(sumPaymentTotal('billAmt')).toEqual(200);
 
         billAmtInput.value = 500;
         tipAmtInput.value = 100;
         submitPaymentInfo();
         expect(sumPaymentTotal('billAmt')).toEqual(700);
+    });
+
+    it('should add current and previous tip percentages', function () {
+        expect(sumPaymentTotal('tipPercent')).toEqual(20);
+
+        billAmtInput.value = 100;
+        tipAmtInput.value = 40;
+        submitPaymentInfo();
+        expect(sumPaymentTotal('tipPercent')).toEqual(60);
+    });
+
+    it('should calculate the tip percent on a single payment', function () {
+        expect(calculateTipPercent(200, 15)).toEqual(8);
+        expect(calculateTipPercent(250, 100)).toEqual(40);
+    });
+
+    it('should create a new td from value and append to the existing table', function () {
+        let row = document.createElement('tr');
+        appendTd(row, 'appended');
+        //expect(row.children.length).not.toEqual(0); 
+        expect(row.children.length).toEqual(1);
+        expect(row.firstChild.innerText).toEqual('appended');
+    });
+
+    it('should create a td with X and append to the existing table', function () {
+        let row = document.createElement('tr');
+        appendDeleteBtn(row);
+        expect(row.children.length).toEqual(1);
+        expect(row.firstChild.innerText).toEqual('X');
     });
 
     afterEach(function () {
